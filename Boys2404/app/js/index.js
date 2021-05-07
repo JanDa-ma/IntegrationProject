8e3cf54c99a66f24bb85e8d9cd8df2e4a7f21490
PaterJejan <iejan.massart@student.odisee.be>
2021-05-07 14:46:25 +0200

    mapjes gemaakt

--- Boys2404/app/js/index.js
+++ Boys2404/app/js/index.js
@@ -1,8 +1,17 @@
 function zegHalloTegenSap() {
-  alert("Oh, wat ben ik kut");
+  alert("Werkt nog niet");
 
   let db = new sqlite3.Database("../../onkostendb.db");
   consolelog("db");
   consolelog(db);
   window.location.href = "admin/OnkostenType?$top=11";
+}
+function Inloggen() {
+  username = document.getElementById("username").textContent;
+  password = document.getElementById("password").textContent;
+  if (username && password == "admin") {
+    window.location.href = "admin/admin.html";
+  } else if (username && password == "user") {
+    window.location.href = "user/user.html";
+  }
 }
