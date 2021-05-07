e82fbd33ad691ca3262c55285956cb2a78c3f111
PaterJejan <iejan.massart@student.odisee.be>
2021-05-07 16:23:16 +0200

    overzicht bedrijven,medewerkers af, onkosten 50% af

---
+++ Boys2404/app/admin/admin.js
@@ -0,0 +1,35 @@
+function Flexso() {
+  cardMedewerkersMeetup = document.getElementById("cardMedewerkersMeetup");
+  cardMedewerkersOdisee = document.getElementById("cardMedewerkersOdisee");
+  cardMedewerkersFlexso = document.getElementById("cardMedewerkersFlexso");
+  cardOnkosten = document.getElementById("cardOnkosten");
+  cardOnkosten.style = "display: none;";
+  cardMedewerkersFlexso.style = "display: inline-block;";
+  cardMedewerkersOdisee.style = "display: none;";
+  cardMedewerkersMeetup.style = "display: none;";
+}
+function Odisee() {
+  cardMedewerkersMeetup = document.getElementById("cardMedewerkersMeetup");
+  cardMedewerkersOdisee = document.getElementById("cardMedewerkersOdisee");
+  cardMedewerkersFlexso = document.getElementById("cardMedewerkersFlexso");
+  cardOnkosten = document.getElementById("cardOnkosten");
+  cardOnkosten.style = "display: none;";
+  cardMedewerkersOdisee.style = "display: inline-block;";
+  cardMedewerkersFlexso.style = "display: none;";
+  cardMedewerkersMeetup.style = "display: none;";
+}
+function Meetup() {
+  cardMedewerkersMeetup = document.getElementById("cardMedewerkersMeetup");
+  cardMedewerkersOdisee = document.getElementById("cardMedewerkersOdisee");
+  cardMedewerkersFlexso = document.getElementById("cardMedewerkersFlexso");
+  cardOnkosten = document.getElementById("cardOnkosten");
+  cardOnkosten.style = "display: none;";
+  cardMedewerkersMeetup.style = "display: inline-block;";
+  cardMedewerkersOdisee.style = "display: none;";
+  cardMedewerkersFlexso.style = "display: none;";
+}
+function ToonOnkosten() {
+  cardOnkosten = document.getElementById("cardOnkosten");
+  cardOnkosten.style = "display: inline-block;";
+  console.log("onkosten");
+}
