e670bdca1ed69732dc32d5acbc928253ae1f4df1
PaterJejan <iejan.massart@student.odisee.be>
2021-05-07 14:28:26 +0200

    Changes dat jan had gemaakt maar kwamen er niet door

--- Boys2404/app/js/index.js
+++ Boys2404/app/js/index.js
@@ -1,8 +1,8 @@
-sap.ui.define([
+function zegHalloTegenSap() {
+  alert("Oh, wat ben ik kut");
 
-], function () {
-	"use strict";
-
-
-	alert("UI5 is ready");
-});
+  let db = new sqlite3.Database("../../onkostendb.db");
+  consolelog("db");
+  consolelog(db);
+  window.location.href = "admin/OnkostenType?$top=11";
+}
